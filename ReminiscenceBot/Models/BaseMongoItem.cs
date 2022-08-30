6858cc054f8db6066893e375eb9426f32b3be47c
larsdekwant <Larskwant@gmail.com>
2022-08-30 11:57:34 +0200

    add abstract base for mongodb items

---
+++ ReminiscenceBot/Models/BaseMongoItem.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace ReminiscenceBot.Models
+{
+    /// <summary>
+    /// MongoDB entity base class, which contains a field to store the MongoDB id.
+    /// </summary>
+    public abstract record BaseMongoItem
+    {
+        /// <summary>
+        /// This field is used by the MongoDB driver to (de)serialize MongoDB documents.
+        /// </summary>
+        [BsonId, BsonIgnoreIfDefault]
+        private ObjectId _mongoId;
+    } 
+}
